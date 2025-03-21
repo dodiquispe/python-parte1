89e7df567a60ad39695312fc573a1dc1bfbc83df
dodiquispe <juditcoyoquispe@gmail.com>
2025-03-21 11:34:40 -0400

    ejercicio 5 comletado

---
+++ ejercicio5.py
@@ -0,0 +1,16 @@
+'''Ejercicio 5
+Escribir un programa que pida al usuario un número entero mayor a 0 (se debe validar el número) y se calcule el factorial de ese número.
+el factorial de un número n se calcula multiplicando los números enteros empezando desde 1 hasta el número n ejemplo:.
+5! = 5 × 4 × 3 × 2 × 1 = 120
+Respuesta Esperada:
+Introduce un número entero:  5
+El factorial de 5 es: 120'''
+
+factorial=1
+numero = int(input('ingrese un numero entero mayor a cero'))
+if numero >= 0:
+    for i in range(numero, 0, -1):
+        factorial*=i
+    print (factorial)
+else:
+    print("no es mayor a cero")
